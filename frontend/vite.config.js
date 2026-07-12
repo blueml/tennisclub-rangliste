2514905fda4e1c649d12faa462fa3737b6583e9e
Marcus Blüml <blueml@blueml-it.de>
2026-07-12 20:22:10 +0200

    Add allowed hosts

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 4000,
+    allowedHosts: ['annotationist.de', 'comunicar.de'],
     proxy: {
       '/api': {
         target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:8080',
