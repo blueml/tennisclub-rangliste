aa524c8e7742e048ed2043c9703257e7c65c381e
Marcus Blüml <blueml@blueml-it.de>
2026-07-12 18:00:13 +0200

    User port 80

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
+    port: 80,
     proxy: {
       '/api': {
         target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:8080',
